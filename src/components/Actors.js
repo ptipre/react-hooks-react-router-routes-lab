9047ae9a3707c549326fcae7694434c182948f3a
Prajay Tipre <prajaytipre@gmail.com>
2021-12-07 16:38:17 -0500

    commit tree 692af7ecde0ca00508c904feda6b07ff204a639a

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const actorsList = actors.map((actor) => {
+    return <div key={actor.name}>{actor.name}
+      <ul>
+        {actor.movies.map(((movie) => {
+          return <li key={movie}>{movie}</li>
+        }))}
+      </ul>
+    </div>
+  })
+
+  return <div>
+    <h1>Actors Page</h1>
+    {actorsList}</div>;
+
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const movieList = movies.map((movie) => {
+    return <div key={movie.title}>{movie.title} {movie.time}
+      <ul>
+        {movie.genres.map(((genre) => {
+          return <li key={genre}>{genre}</li>
+        }))}
+      </ul>
+    </div>
+  })
+
+  return <div>
+    <h1>Movies Page</h1>
+    {movieList}
+    </div>;
 }
 
 export default Movies;
